ec63dc28a58243759e57def7af068892f2e8113a
Myo T Kyaw <myotkyaw@gmail.com>
2018-01-31 21:17:29 -0800

    Add material ui and implement a controlled tab

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MUITestComponent from './MUITestComponent';
 import logo from './logo.svg';
 import './App.css';
 
@@ -13,6 +15,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <MuiThemeProvider>
+          <MUITestComponent />
+        </MuiThemeProvider>
+
       </div>
     );
   }
---
+++ src/MUITestComponent.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {Tabs, Tab} from 'material-ui/Tabs';
+
+const styles = {
+  headline: {
+    fontSize: 24,
+    paddingTop: 16,
+    marginBottom: 12,
+    fontWeight: 400,
+  },
+};
+
+export default class MUITestComponent extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'a',
+    };
+  }
+
+  handleChange = (value) => {
+    this.setState({
+      value: value,
+    });
+  };
+
+  render() {
+    return (
+      <Tabs
+        value={this.state.value}
+        onChange={this.handleChange}
+      >
+        <Tab label="Tab A" value="a">
+          <div>
+            <h2 style={styles.headline}>Controllable Tab A</h2>
+            <p>
+              Tab 1 content here.
+            </p>
+          </div>
+        </Tab>
+        <Tab label="Tab B" value="b">
+          <div>
+            <h2 style={styles.headline}>Controllable Tab B</h2>
+            <p>
+              Tab 2 content here.
+            </p>
+          </div>
+        </Tab>
+      </Tabs>
+    );
+  }
+}

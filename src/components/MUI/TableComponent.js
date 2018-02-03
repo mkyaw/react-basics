e064668eea8f5ff03c7ab76fcc230ebbe77e2c47
Myo T Kyaw <myotkyaw@gmail.com>
2018-02-02 18:25:12 -0800

    Update table component to use the user props passed down to populate the table data

--- src/components/MUI/TableComponent.js
+++ src/components/MUI/TableComponent.js
@@ -7,45 +7,30 @@ import {
   TableRow,
   TableRowColumn,
 } from 'material-ui/Table';
+import {Face,Email, Assignment} from 'material-ui-icons';
 
 /**
  * A simple table demonstrating the hierarchy of the `Table` component and its sub-components.
  */
-const TableExampleSimple = () => (
+const TableExampleSimple = (props) => (
   <Table>
     <TableHeader>
       <TableRow>
-        <TableHeaderColumn>ID</TableHeaderColumn>
-        <TableHeaderColumn>Name</TableHeaderColumn>
-        <TableHeaderColumn>Status</TableHeaderColumn>
+        <TableHeaderColumn><Assignment /><br />ID</TableHeaderColumn>
+        <TableHeaderColumn><Face /><br />Name</TableHeaderColumn>
+        <TableHeaderColumn><Email /><br />Email</TableHeaderColumn>
       </TableRow>
     </TableHeader>
     <TableBody>
-      <TableRow>
-        <TableRowColumn>1</TableRowColumn>
-        <TableRowColumn>John Smith</TableRowColumn>
-        <TableRowColumn>Employed</TableRowColumn>
-      </TableRow>
-      <TableRow>
-        <TableRowColumn>2</TableRowColumn>
-        <TableRowColumn>Randal White</TableRowColumn>
-        <TableRowColumn>Unemployed</TableRowColumn>
-      </TableRow>
-      <TableRow>
-        <TableRowColumn>3</TableRowColumn>
-        <TableRowColumn>Stephanie Sanders</TableRowColumn>
-        <TableRowColumn>Employed</TableRowColumn>
-      </TableRow>
-      <TableRow>
-        <TableRowColumn>4</TableRowColumn>
-        <TableRowColumn>Steve Brown</TableRowColumn>
-        <TableRowColumn>Employed</TableRowColumn>
-      </TableRow>
-      <TableRow>
-        <TableRowColumn>5</TableRowColumn>
-        <TableRowColumn>Christopher Nolan</TableRowColumn>
-        <TableRowColumn>Unemployed</TableRowColumn>
-      </TableRow>
+        {props.users.map((user) => {
+          return (
+            <TableRow key={user.key}>
+              <TableRowColumn>{user.id}</TableRowColumn>
+              <TableRowColumn>{user.name}</TableRowColumn>
+              <TableRowColumn>{user.email}</TableRowColumn>
+            </TableRow>
+          )
+        })}
     </TableBody>
   </Table>
 );

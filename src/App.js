b5e20786e4d33430c204cabccb16b5ee90095904
Myo T Kyaw <myotkyaw@gmail.com>
2018-02-07 13:28:56 -0800

    Remove 'return' from AppBody component and wrap it with '(...)'

--- src/App.js
+++ src/App.js
@@ -2,37 +2,36 @@ import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import TabComponent from './components/MUI/TabComponent';
 import axios from 'axios';
+
 // ** TEST COMPONENT
 // import TestComponent from './components/TestComponent';
 
 import logo from './logo.svg';
 import './App.css';
 
-const AppBody = ({input, users, dummyUsers}) => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React!!!</h1>
-      </header>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-      <MuiThemeProvider>
-        <div onChange={this.onTextInputChange}>
-          <TabComponent input={input} users={users} dummyUsers={dummyUsers} />
-        </div>
-      </MuiThemeProvider>
-      
-      {/* TEST COMPONENT */}
-      {/*
-      <MuiThemeProvider>
-        <TestComponent users={this.state.users}/>
-      </MuiThemeProvider>
-      */}
-    </div>
-  );
-}
+const AppBody = ({input, users, dummyUsers}) => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React!!!</h1>
+    </header>
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+    <MuiThemeProvider>
+      <div onChange={this.onTextInputChange}>
+        <TabComponent input={input} users={users} dummyUsers={dummyUsers} />
+      </div>
+    </MuiThemeProvider>
+    
+    {/* TEST COMPONENT */}
+    {/*
+    <MuiThemeProvider>
+      <TestComponent users={this.state.users}/>
+    </MuiThemeProvider>
+    */}
+  </div>
+)
 
 class App extends Component {
   constructor(props) {
